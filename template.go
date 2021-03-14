abc9bf2b67c1fe81405763cb42a969e16b7be9d1
hadidharmawan <hadidharmawan32@gmail.com>
2021-03-14 23:56:10 +0700

    add code func homepage and pagevariables

--- template.go
+++ template.go
@@ -18,5 +18,20 @@ func main() {
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request){
-	
+
+	now := time.Now()
+	HomePageVars := PageVariables{
+		Date: now.Format("02-01-2006"),
+		Time: now.Format("15:04:05"),
+	}
+
+	t, err := template.ParseFiles("homepage.html")
+	if err != nil {
+		log.Print("template parsing error: ", err)
+	}
+
+	err = t.Execute(w, HomePageVars)
+	if err != nil {
+		log.Print("template executing error : ", err)
+	}
 }
